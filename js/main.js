64b5f461b517db9af12dbeb20982dc8ed3b70538
Ma <r_ma3@fanshawec.ca>
2017-11-24 14:50:43 -0500

    this is the A3_Lab1

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function () {
+  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
+   var theImages = document.querySelectorAll('.data-ref'),
+    carModel = document.querySelector('.modelName'),
+    price = document.querySelector('.priceInfo'),
+    carDetail = document.querySelector('.modelDetails'),
+    appliedClass;
+
+    function changeElements() {
+
+    let carIndex = carData[this.id];
+
+    carModel.firstChild.nodeValue = carIndex.headline;
+    price.firstChild.nodeValue = carIndex.price;
+    carDetail.firstChild.nodeValue = carIndex.text;
+
+   theImages.forEach(function(element, index){
+     element.classList.add('nonActive');
+   })
+
+  this.classList.remove('nonActive');
+}
+
+  theImages.forEach(function(element, index) {
+
+  element.addEventListener('click', changeElements, false);
+
+})
+
+})();
